f16435ecb896188c84ecaa110fda0a4a2f307c54
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-11-29 02:23:41 +0900

    Use || to handle curl error occurence

--- alert_webhook_script_on_Splunk.sh
+++ alert_webhook_script_on_Splunk.sh
@@ -65,9 +65,17 @@ App_Header=$App_Header_Key":"$App_Header_Value
 
 echo "trying to curl" >> script.log
 
-command="curl -H \"$App_Header\" -H \"Content-Type:application/json\" -d \"{'SPLUNK_ARG_0':'$script_name', 'SPLUNK_ARG_1':'$number_of_events', 'SPLUNK_ARG_2':'$search_terms', 'SPLUNK_ARG_3':'$query_string', 'SPLUNK_ARG_4':'$name_of_report', 'SPLUNK_ARG_5':'$trigger_reason', 'SPLUNK_ARG_6':'$browser_url'}\" https://stg-csharptestfunction.azurewebsites.net/api/AlertToViber >> script.log 2>&1"
+# fail
+command="curl -H \"$App_Header\" -H \"Content-Type:application/json\" -d \"{'SPLUNK_ARG_0':'$script_name', 'SPLUNK_ARG_1':'$number_of_events', 'SPLUNK_ARG_2':'$search_terms', 'SPLUNK_ARG_3':'$query_string', 'SPLUNK_ARG_4':'$name_of_report', 'SPLUNK_ARG_5':'$trigger_reason', 'SPLUNK_ARG_6':'$browser_url'}\" https://stg-csharptestfunctionasdfasdfadfdaddad.azurewebsites.net/api/AlertToViber >> script.log 2>&1"
 
-eval ${command}
+# success
+command2="curl -H \"$App_Header\" -H \"Content-Type:application/json\" -d \"{'SPLUNK_ARG_0':'$script_name', 'SPLUNK_ARG_1':'$number_of_events', 'SPLUNK_ARG_2':'$search_terms', 'SPLUNK_ARG_3':'$query_string', 'SPLUNK_ARG_4':'$name_of_report', 'SPLUNK_ARG_5':'$trigger_reason', 'SPLUNK_ARG_6':'$browser_url'}\" https://stg-csharptestfunction.azurewebsites.net/api/AlertToViber >> script.log 2>&1"
+
+# fail
+command3="curl -H \"$App_Header\" -H \"Content-Type:application/json\" -d \"{'SPLUNK_ARG_0':'$script_name', 'SPLUNK_ARG_1':'$number_of_events', 'SPLUNK_ARG_2':'$search_terms', 'SPLUNK_ARG_3':'$query_string', 'SPLUNK_ARG_4':'$name_of_report', 'SPLUNK_ARG_5':'$trigger_reason', 'SPLUNK_ARG_6':'$browser_url'}\" https://stg-csharpalsalskjflajsdlfajdftestfunction.azurewebsites.net/api/AlertToViber >> script.log 2>&1"
+
+# command2 runs
+eval ${command} || eval ${command2} || eval ${command3}
 
 echo "curl end" >> script.log
 
